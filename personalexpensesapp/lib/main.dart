420a1f589d6d705b1c301b6c85e6f28a01d58190
Thiruvikraman1998 <development.thiru@gmail.com>
2023-04-03 13:13:53 +0530

    initial commit

---
+++ personalexpensesapp/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
